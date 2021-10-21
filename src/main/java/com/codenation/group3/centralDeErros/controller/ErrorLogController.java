2fa1b87a3f4b412f2f88a62d96a01473d7d31f09
Henrique Brito Elias <henriquebelias@gmail.com>
2021-10-21 10:34:09 -0300

    Implemented the cros feature

--- src/main/java/com/codenation/group3/centralDeErros/controller/ErrorLogController.java
+++ src/main/java/com/codenation/group3/centralDeErros/controller/ErrorLogController.java
@@ -1,9 +1,7 @@
 package com.codenation.group3.centralDeErros.controller;
 
 import com.codenation.group3.centralDeErros.dtos.ErrorLogDTO;
-import com.codenation.group3.centralDeErros.dtos.UserDTO;
 import com.codenation.group3.centralDeErros.entity.ErrorLog;
-import com.codenation.group3.centralDeErros.entity.User;
 import com.codenation.group3.centralDeErros.service.ErrorLogService;
 
 import org.springframework.data.domain.Page;
@@ -17,6 +15,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/error-log")
+@CrossOrigin
 public class ErrorLogController {
     ErrorLogService errorLogService;
 
